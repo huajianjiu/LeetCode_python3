99596c38189642e90884d956d04c7935a804b91c
Yuanzhi Ke <lastranger8811@gmail.com>
2019-03-26 01:15:11 +0900

    Create twoSum.py

---
+++ twoSum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        index_number = {}
+        for i, v in enumerate(nums):
+            try:
+                j = index_number[target-v]
+                if i>j:
+                    return [j,i]
+                else:
+                    return [i,j]
+            except KeyError:
+                index_number[v]=i
+                continue
